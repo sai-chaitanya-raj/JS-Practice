d61765a5e612718fc7dbb03dc969d815d7bc469d
RAJ <peketisaichaitanyaraj@gmail.com>
2025-08-22 09:41:12 +0530

    skanke water gun

--- Saradha.js
+++ Saradha.js
@@ -1,2 +1,13 @@
-console.log("hihello 1")
-console.log(console)
+// let prompt = require("prompt-sync")();
+let input = prompt("Enter one of these Snake,Water,Gun");
+let versus = ["SNAKE","WATER","GUN"];
+let random = Math.floor(Math.random()*3);
+let capran = versus[random].toUpperCase(); 
+let cap = input.toUpperCase()
+switch(true){
+     case cap === capran:
+        console.log("Salaar U got it!!");
+        break;
+    default:
+        console.log("Ahh U missed it babe");
+}
